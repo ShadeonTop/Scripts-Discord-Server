564945d4e4cd51385d00fe059fa7658f0727b1ab
ShadeonTop <149391917+ShadeonTop@users.noreply.github.com>
2023-11-02 16:14:02 +0100

    Delete -beta-/main/Wiihub.lua

--- -beta-/main/Wiihub.lua
+++
@@ -1,5 +0,0 @@
-script_key="AdFIDwKRqcoUiFhrAYTlCvJOtIPuxJjk";
-loadstring(game:HttpGet("https://raw.githubusercontent.com/vFishyTurtle/wiihub/main/free%20loader"))()
-
-
-Key Need to do Yourself : https://wiihub.net/trial
